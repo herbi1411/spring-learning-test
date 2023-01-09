2e6392f3012bfa4682f8201395d663a917fdd43b
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-09 15:54:51 +0900

    core done

--- src/main/java/nextstep/helloworld/core/di/StationSetterService.java
+++ src/main/java/nextstep/helloworld/core/di/StationSetterService.java
@@ -3,6 +3,11 @@ package nextstep.helloworld.core.di;
 public class StationSetterService {
     private StationRepository stationRepository;
 
+    @Autowired
+    public void setStationRepository(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
     public String sayHi() {
         return stationRepository.sayHi();
     }
--- src/main/java/nextstep/helloworld/core/scan/LineDao.java
+++ src/main/java/nextstep/helloworld/core/scan/LineDao.java
@@ -1,4 +1,8 @@
 package nextstep.helloworld.core.scan;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class LineDao {
 }
