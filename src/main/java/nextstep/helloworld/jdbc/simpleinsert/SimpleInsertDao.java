bb87cf94fb301176d19b78092711bc591247c2dc
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-09 15:43:27 +0900

    jdbc done

--- src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
+++ src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
@@ -5,6 +5,8 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class SimpleInsertDao {
@@ -22,7 +24,11 @@ public class SimpleInsertDao {
      * id를 포함한 Customer 객체를 반환하세요
      */
     public Customer insertWithMap(Customer customer) {
-        return null;
+        Map<String, Object> map = new HashMap<>();
+        map.put("first_name", customer.getFirstName());
+        map.put("last_name", customer.getLastName());
+        long id = insertActor.executeAndReturnKey(map).longValue();
+        return new Customer(id, customer.getFirstName(), customer.getLastName());
     }
 
     /**
