024dd200daed91bd2e29a53da30c84f35136922c
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-09 14:55:01 +0900

    mvc done

--- src/main/java/nextstep/helloworld/mvc/exceptions/HelloAdvice.java
+++ src/main/java/nextstep/helloworld/mvc/exceptions/HelloAdvice.java
@@ -4,8 +4,9 @@ import nextstep.helloworld.mvc.exceptions.exception.HelloException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+@ControllerAdvice
 public class HelloAdvice {
+    @ExceptionHandler(HelloException.class)
     public ResponseEntity<String> handle() {
         return ResponseEntity.badRequest().body("HelloException");
     }
--- src/main/java/nextstep/helloworld/mvc/mapping/UriPatternController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/UriPatternController.java
@@ -2,22 +2,24 @@ package nextstep.helloworld.mvc.mapping;
 
 import nextstep.helloworld.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/uri-pattern")
 public class UriPatternController {
 
-    public ResponseEntity<User> pathVariable(Long id) {
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> pathVariable(@PathVariable Long id) {
         User user = new User(id, "이름", "email");
         return ResponseEntity.ok().body(user);
     }
 
+    @GetMapping("/patterns/?")
     public ResponseEntity<String> pattern() {
         return ResponseEntity.ok().body("pattern");
     }
 
+    @GetMapping("/patterns/**")
     public ResponseEntity<String> patternStars() {
         return ResponseEntity.ok().body("pattern-multi");
     }
