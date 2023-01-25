fd439dbb33421a7f8d70caf3b68d4de1d15f298e
herbi.sf8 <herbi.sf8@kakaocorp.com>
2023-01-25 18:32:18 +0900

    config done

--- src/main/java/nextstep/helloworld/config/environment/PropertySourceConfig.java
+++ src/main/java/nextstep/helloworld/config/environment/PropertySourceConfig.java
@@ -1,19 +1,29 @@
 package nextstep.helloworld.config.environment;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 // TODO: Java-based Configuration을 하기 위한 클래스로 지정하기
 // TODO: application.properties 파일을 활용하기 위한 설정 추가하기
+@Configuration
+@PropertySource("application.properties")
 public class PropertySourceConfig {
 
     private final Environment env;
+
+    @Value("${security-jwt-token-secret-key}")
+    private String secretKey;
 
     public PropertySourceConfig(Environment env) {
         this.env = env;
     }
 
     // TODO: application.properties의 security-jwt-token-secret-key 값을 활용하여 JwtTokenKeyProvider를 빈으로 등록하기
+    @Bean
     public JwtTokenKeyProvider jwtTokenKeyProvider() {
-        return new JwtTokenKeyProvider("");
+        return new JwtTokenKeyProvider(secretKey);
     }
 }
